0dc981320af8e7be8bbc7c22095d43f5af3f5518
Kolya <116844454+playsus@users.noreply.github.com>
2023-04-18 23:09:42 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        header('location: ../error.php');
+        exit;
+    }
+
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'password123');
+?>
